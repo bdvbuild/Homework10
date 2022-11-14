f74fd97723880895591332c95efca8fabc7e7033
Dmitriy Baranov <bdvbuild@gmail.com>
2022-11-14 23:53:45 +0400

    Добавьте файлы проекта.

---
+++ Homework10/Angle.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework10
+{
+    internal class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public bool isCorrect = true;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (gradus < 0)
+                    isCorrect = false;
+                else
+                    gradus = value % 360;
+            }
+        }
+
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (min < 0)
+                    isCorrect = false;
+                else
+                {
+                    min = value % 60;
+                    Gradus += value / 60;
+                }
+            }
+        }
+
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (min < 0)
+                    isCorrect = false;
+                else
+                {
+                    sec = value % 60;
+                    Min += value / 60;
+                }
+            }
+        }
+        public Angle(int gradus, int min, int sec)
+        {
+            Gradus = gradus;
+            Min = min;
+            Sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            return gradus * (Math.PI / 180) + min * (Math.PI /( 180 * 60)) + sec * (Math.PI / (180 * 60 * 60));
+        }
+    }
+}
---
+++ Homework10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд.
+//Реализовать класс, в котором указанные значения представлены в виде свойств.
+//Для свойств предусмотреть проверку корректности данных.
+//Класс должен содержать конструктор для установки начальных значений, а также метод ToRadians для перевода угла в радианы.
+//Создать объект на основе разработанного класса. Осуществить использование объекта в программе.
+
+
+
+namespace Homework10
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle;
+            do
+            {
+                    Console.Write("Введите градусы: ");
+                    int gradus_ = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите минуты: ");
+                    int min_ = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите секунды: ");
+                    int sec_ = Convert.ToInt32(Console.ReadLine());
+
+                    angle = new Angle(gradus_, min_, sec_);
+
+                if (!angle.isCorrect)
+                    Console.WriteLine("\nОшибка. Повторите ввод.");
+
+            } while (angle.isCorrect == false);
+
+            double radian = angle.ToRadians();
+            Console.Write($"Радианы: {radian}");
+            Console.ReadLine();
+        }
+    }
+}
